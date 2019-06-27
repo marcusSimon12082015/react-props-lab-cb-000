77e06b7e720cc892fc4356469d0f862f6474a4ca
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-27 11:41:44 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -3,5 +3,14 @@ import React from 'react';
 
 export default class Spaceship extends React.Component
 {
-
+  render(){
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.speed}</p>
+        <p>{this.props.hasRockets}</p>
+        <p>{this.props.colors.join(', ')}</p>
+      </div>
+    )
+  }
 }
